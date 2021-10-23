d3810415c06866b212f29357f6b362fd8b35a2ed
Kirill Kuzmin <quezee@yandex.ru>
2021-10-23 14:09:59 +0300

    remade validation imgsz for models 01 and 02

--- val.sh
+++ val.sh
@@ -1,12 +1,12 @@
 python /workspace/yolov5/val.py \
     --data data/dataset.yaml \
-    --weights runs/train/model03/weights/best.pt \
-    --batch-size 10 \
-    --imgsz 1500 \
-    --conf-thres 0.312 \
+    --weights runs/train/model01/weights/best.pt \
+    --batch-size 12 \
+    --imgsz 1920 \
+    --conf-thres 0.332 \
     --iou-thres 0.6 \
     --task test \
     --device 0 \
     --verbose \
     --project /workspace/project/runs/val \
-    --name model03
+    --name model01
