1a07133058da6c003f8585f03a7825d968e983ce
Kirill Kuzmin <quezee@yandex.ru>
2021-12-09 23:46:08 +0300

    fixed wrong imgsz setting

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 python yolov5/export.py \
     --weights runs/train/model03/weights/best.pt \
-    --imgsz 1920 \
+    --imgsz 1280 1920 \
     --device 0 \
     --conf-thres 0.454 \
     --iou-thres 0.6 \
